9b60b7ef76cf39574436e5d6cad15c8ba42f8e87
hana <asufanah@gmail.com>
2015-04-05 20:44:46 +0900

    Modify test code.

--- src/test/java/com/github/asufana/waf/WAFLiteTest.java
+++ src/test/java/com/github/asufana/waf/WAFLiteTest.java
@@ -12,48 +12,38 @@ import com.github.asufana.waf.testutils.Http.HttpResponse;
 
 public class WAFLiteTest {
     
-    private final Integer port = 18888;
-    private final WAFLite waf = new WAFLite(port);
+    private final Integer port = 8888;
+    private Server server;
     
-    @Test
-    public void testServerStartAndStop() throws Exception {
-        final Server server = waf.start();
+    @Before
+    public void before() throws Exception {
+        server = new WebApp(port).start();
         assertThat(Netstat.isPortOpen(port), is(true));
-        
+    }
+    
+    @After
+    public void after() throws Exception {
         server.stop();
         assertThat(Netstat.isPortOpen(port), is(false));
     }
     
     @Test
     public void testRouteNotFound() throws Exception {
-        final Server server = new WebApp(port).start();
-        
         assertThat(Http.get(port, "/no_route").code(), is(404));
-        
-        server.stop();
     }
     
     @Test
     public void testRoutingWithoutParams() throws Exception {
-        final Server server = new WebApp(port).start();
-        
-        //exists route
         final HttpResponse res = Http.get(port, "/hello");
         assertThat(res.code(), is(200));
         assertThat(res.contents(), is("Hello!"));
-        
-        server.stop();
     }
     
     @Test
     public void testRoutingWithOneParam() throws Exception {
-        final Server server = new WebApp(port).start();
-        
         final HttpResponse res = Http.get(port, "/hello/user/hana");
         assertThat(res.code(), is(200));
         assertThat(res.contents(), is("Hello hana"));
-        
-        server.stop();
     }
     
     public static class WebApp extends WAFLite {
